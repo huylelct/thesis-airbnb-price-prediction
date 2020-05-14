8fb0619b0ec0f0f974fdce1d60d66c2aadd74661
Huy Le <huyld@zvota.com.vn>
2020-05-14 11:27:56 +0700

    init run model code, report about init model

--- main/feature_selection.py
+++ main/feature_selection.py
@@ -12,18 +12,38 @@ bestfeatures = SelectKBest(score_func=f_regression, k=top)
 fit = bestfeatures.fit(data_x, data_y)
 dfscores = pd.DataFrame(fit.scores_)
 dfcolumns = pd.DataFrame(data_x.columns)
+
 featureScores = pd.concat([dfcolumns, dfscores], axis=1)
 featureScores.columns = ['Specs', 'Score']
-print(featureScores.nlargest(top, 'Score'))
+featureScores = featureScores.sort_values('Score', ascending=False)
+print(featureScores)
+np.save('../data/selected_feature.npy', [
+    'bathrooms',
+    'bedrooms',
+    'accommodates',
+    'beds',
+    'review_scores_rating',
+    'host_response_rate',
+    'room_type',
+    'host_response_time',
+    'reviews_per_month',
+    'latitude',
+    'longitude',
+    'number_of_reviews',
+    'calculated_host_listings_count_private_rooms',
+    'instant_bookable'
+])
 
-corrmat = data_x.corr()
-top_corr_features = corrmat.index
-sns.heatmap(data_x[top_corr_features].corr(), annot=True, cmap="RdYlGn")
+# print(featureScores.sort_values('Score'))
 
-columns = np.full((corrmat.shape[0],), True, dtype=bool)
-for i in range(corrmat.shape[0]):
-    for j in range(i + 1, corrmat.shape[0]):
-        if corrmat.iloc[i, j] >= 0.9:
-            if columns[j]:
-                print(data_x.columns[i], data_x.columns[j])
-                columns[j] = False
+# corrmat = data_x.corr()
+# top_corr_features = corrmat.index
+# sns.heatmap(data_x[top_corr_features].corr(), annot=True, cmap="RdYlGn")
+#
+# columns = np.full((corrmat.shape[0],), True, dtype=bool)
+# for i in range(corrmat.shape[0]):
+#     for j in range(i + 1, corrmat.shape[0]):
+#         if corrmat.iloc[i, j] >= 0.9:
+#             if columns[j]:
+#                 print(data_x.columns[i], data_x.columns[j])
+#                 columns[j] = False
